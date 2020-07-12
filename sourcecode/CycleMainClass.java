188b2bd232faddda6b73d7ebf7b5dcf4c40f631f
Ajay1735 <ajayvallala1735@gmail.com>
2020-07-12 23:08:45 +0530

    Updated CycleMainClass 	- To test the current system 	- Creates 2 cycles with 2 sets of gear and wheel combination

--- sourcecode/CycleMainClass.java
+++ sourcecode/CycleMainClass.java
@@ -6,14 +6,26 @@ java -cp classFiles cycleOops.CycleMainClass
 */
 
 public class CycleMainClass {
-
+    
     public static void main(String[] args) {
-        Gear g1 = new Gear(52, 11);
-        System.out.println(g1.getRatio());
+        Gear gear1 = new Gear(52, 11);
+        System.out.println(gear1.getRatio());
 
-        Gear g2 = new Gear(30, 27);
-        System.out.println(g2.getRatio());
+        Gear gear2 = new Gear(30, 27);
+        System.out.println(gear2.getRatio());
 
+        Wheel wheel1 = new Wheel(26, 1.5);
+        System.out.println(wheel1.getDiameter());
+        
+        Wheel wheel2 = new Wheel(24, 1.25);
+    	System.out.println(wheel2.getDiameter());
+
+    	Cycle cycle1 = new Cycle(gear1, wheel1);
+        System.out.println(cycle1.getGearInches());
+        
+        Cycle cycle2 = new Cycle(gear1, wheel2);
+        System.out.println(cycle2.getGearInches());
+    
     }
 
 }
